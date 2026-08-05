e5e1001808be0cb91287f243028beae2ae8ff9a7
cpejuniorcollege <mastigaming927@gmail.com>
2026-08-05 09:49:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ reveals.forEach((item)=>{
     observer.observe(item);
 
 });
+// Scroll reveal animation
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach((element) => {
+
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const revealPoint = 120;
+
+        if(elementTop < windowHeight - revealPoint){
+            element.classList.add("active");
+        }
+
+    });
+
+});

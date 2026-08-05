df0480b15e3bede9e58129c3ae568fde4a51b5a4
cpejuniorcollege <mastigaming927@gmail.com>
2026-08-05 10:03:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,25 @@ window.addEventListener("scroll", () => {
     });
 
 });
+// Navbar scroll effect
+
+const navbar = document.querySelector(".navbar");
+
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY > 50){
+
+        navbar.style.background =
+        "rgba(0,0,0,0.55)";
+
+    }
+
+    else{
+
+        navbar.style.background =
+        "rgba(255,255,255,0.05)";
+
+    }
+
+});

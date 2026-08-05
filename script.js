f9355e6122d2f94dce45e51e160e17958a435820
cpejuniorcollege <mastigaming927@gmail.com>
2026-08-05 09:26:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const reveals = document.querySelectorAll(".reveal");
 
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("active");
+
+        }
+
+    });
+
+},{
+    threshold:0.15
+});
+
+reveals.forEach((item)=>{
+
+    observer.observe(item);
+
+});
